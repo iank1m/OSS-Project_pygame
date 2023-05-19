3a42f4b94964e2c2495a50b52b79b7260b417687
김지원 <127467108+gamjawon@users.noreply.github.com>
2023-05-19 13:51:41 +0900

    Update 2_start_screen.py

--- 2_start_screen.py
+++ 2_start_screen.py
@@ -13,9 +13,16 @@ screen_height = 780 # 세로 크기
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Memory Game")
 
+# 배경이미지 불러오기
+background = pygame.image.load("game_background.png")
+# 새로운 화면을 위한 Surface 객체 생성
+sub_screen_width = 865
+sub_screen_height = 520
+sub_screen = pygame.Surface((sub_screen_width, sub_screen_height))
+
 # 시작 버튼
 start_button = pygame.Rect(0, 0, 120, 120)
-start_button.center = (120, screen_height - 120)
+start_button.center = (450, 320)
 
 # 색깔
 BLACK = (0, 0, 0) # RGB 
@@ -29,8 +36,10 @@ while running:
         if event.type == pygame.QUIT: # 창이 닫히는 이벤트인가?
             running = False # 게임이 더 이상 실행중이 아님
 
+    screen.blit(background, (0, 0))  # 배경 그리기
+    screen.blit(sub_screen, (25, 40))  # 새로운 화면을 (30, 30) 위치에 그리기
     # 화면 전체를 까맣게 칠함
-    screen.fill(BLACK)
+    sub_screen.fill(BLACK)
 
     # 시작 화면 표시
     display_start_screen()
